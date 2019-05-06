09fa5dcf1a3581c33a1f1952dd1e4b538be0fde6
Milan <m.drossaerts@gmail.com>
2019-05-06 18:43:59 +0200

    Initial commit

---
+++ IPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HomeCTRL.Plugin
+{
+    public interface IPlugin : IDisposable
+    {
+        /// <summary>
+        /// The startup function where the plugin can do its things.
+        /// Reports true when succesfull; false otherwise
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> Startup();
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# HomeCTRL Plugin Interface (WIP)
+
+This is the plugin interface for HomeCTRL. In order to build your own plugins for HomeCTRL, you need to reference this project and implement these interfaces.
